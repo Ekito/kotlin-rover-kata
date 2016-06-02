17e06be66b5cbddc360fe1669cb58c377bde5d7f
Arnaud Giuliani <agiuliani@ekito.fr>
2016-06-02 10:34:44 +0200

    init repo

---
+++ src/main/kotlin/Rover.kt
@@ -0,0 +1,5 @@
+/**
+ * Created by arnaud on 02/06/2016.
+ */
+data class Rover(var name : String) {
+}
---
+++ src/test/kotlin/RoverTest.kt
@@ -0,0 +1,14 @@
+import org.junit.Test
+
+/**
+ * Created by arnaud on 02/06/2016.
+ */
+class RoverTest {
+
+    @Test
+    fun firstRoverTest() {
+        var rover = Rover("this is me !")
+
+        println("my rover is : ${rover}")
+    }
+}
